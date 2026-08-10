3976bfe9f2a7603eeef34f89cf776517aa954de9
sanvika2102-ops <sanvika2102@gmail.com>
2026-08-10 17:23:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/class10_board/', // Must match your exact GitHub repository name
+  base: '/',
 })
